87d9971b6afba88b883db766eaa6b0978b014778
Jefferson Russi <jeffersonrussi-11@hotmail.com>
2023-06-22 23:30:49 -0300

    voltando a uma versão anterior

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectado ao banco de dados")
-//mais um console log de teste:
-console.log("O paulo fez esse commit sem você ver na aula")
-console.log("Enviando para o Github")
